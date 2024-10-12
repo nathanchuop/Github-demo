d48992523dd17966259058799d330c9aa88b6fd2
hanley76129 <hanhuynh3422@gmail.com>
2024-10-12 10:19:55 -0700

    change

--- src/main.cpp
+++ src/main.cpp
@@ -91,7 +91,7 @@ void application()
   auto& clock = *resources.clock.value();
   auto& console = *resources.console.value();
 
-  hal::print(console, "Starting The Application!\n");
+  hal::print(console, "Starting One Application!\n");
   hal::print(console, "Will reset after ~10 seconds\n");
   //a cool commen something cooler something epic
   for (int i = 0; i < 10; i++) {
