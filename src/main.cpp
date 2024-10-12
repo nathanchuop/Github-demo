b12dd770f3741af6ae5697a82328bfd00db7943c
hanley76129 <hanhuynh3422@gmail.com>
2024-10-12 09:56:18 -0700

    change

--- src/main.cpp
+++ src/main.cpp
@@ -93,7 +93,7 @@ void application()
 
   hal::print(console, "Starting Application!\n");
   hal::print(console, "Will reset after ~10 seconds\n");
-  //a cool comment 
+  //a cool commen something cooler
   for (int i = 0; i < 10; i++) {
     // Print message
     hal::print(console, "Hello, World\n");
